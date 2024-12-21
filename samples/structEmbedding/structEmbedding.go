520ab99ccdb01fc8ac360a8e52d9e923e4048bc3
Rich Davis <vgcrld@gmail.com>
2024-12-21 03:52:27 +0000

    add to base

--- samples/structEmbedding/structEmbedding.go
+++ samples/structEmbedding/structEmbedding.go
@@ -5,11 +5,12 @@ import "fmt"
 // base is a simple struct with a single field num.
 type base struct {
 	num int
+	age int
 }
 
 // describe returns a string representation of the base struct.
 func (b base) describe() string {
-	return fmt.Sprintf("base with num=%v", b.num)
+	return fmt.Sprintf("base with num=%v, age=%v", b.num, b.age)
 }
 
 // container embeds the base struct, meaning it inherits the fields and methods of base.
@@ -26,6 +27,7 @@ func main() {
 	co := container{
 		base: base{
 			num: 1,
+			age: 20,
 		},
 		str: "some name",
 	}
