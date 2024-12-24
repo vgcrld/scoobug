adadea36fbc98360ddb1187588e7a923a1e49860
Rich Davis <vgcrld@gmail.com>
2024-12-24 19:41:43 +0000

    refactor into helps

--- samples/snow/oauth/oauth.go
+++ samples/snow/oauth/oauth.go
@@ -8,85 +8,26 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/vgcrld/scoobug/samples/snow/oauth/helps"
 )
 
-// map[int]string of the status cods and thier meeting for the service now api
-var statusCodes = map[int]string{
-	200: "OK",
-	201: "Created",
-	204: "No Content",
-	400: "Bad Request",
-	401: "Unauthorized",
-	403: "Forbidden",
-	404: "Not Found",
-	405: "Method Not Allowed",
-	415: "Unsupported Media Type",
-	500: "Internal Server Error",
-}
-
-// creds struct contains the necessary credentials for authenticating with the
-type creds struct {
-	oauth_url    string
-	incident_url string
-	grantType    string
-	clientID     string
-	clientSecret string
-	username     string
-	password     string
-}
-
-// token struct contains the fields of the authentication token returned by the
-type tokens struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	Scope        string `json:"scope"`
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int    `json:"expires_in"`
-	Valid        bool
-	RequestCode  int
-}
-
 // used to hold the authentication token
-var accessTokens *tokens
-
-// access_creds contains the credentials for authenticating
-var myCreds creds = creds{
-	oauth_url:    "https://dev256710.service-now.com//oauth_token.do",
-	incident_url: "https://dev256710.service-now.com/api/now/table/incident",
-	grantType:    "password",
-	clientID:     "2eda594ac70345fc80a47c1e5b4c2424",
-	clientSecret: "controlm123",
-	username:     "admin",
-	password:     "KqdT2U2a%5EoR%2B",
-	// password is encoded and needs to be. (same issue nithila saw)
-	// the actual password is 'KqdT2U2a^oR+'  ^ and + are special characters and need to be encoded
-}
+var accessTokens *helps.Tokens
 
 // l is a logger
 var l *log.Logger
-
-// when creating this you have to be careful with the fields you are sending
-// it will trigger a business rule if you are not careful and fail with a 403
-// this failed on a 403 when trying to assign it to a user "admin"
-var snowReq = map[string]string{
-	"short_description": "This is a test incident",
-	"urgency":           "2",
-	"impact":            "2",
-	"category":          "software",
-	"subcategory":       "os",
-	"assignment_group":  "software",
-	// "assigned_to":       "admin",
-}
 
 // main function
 func main() {
 
 	l = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	accessTokens = getTokens(myCreds)
+	c := helps.Creds{}
+	accessTokens = getTokens(c.GetCreds())
 	// loop 4 times
 	for i := 0; i < 4; i++ {
 		if accessTokens.Valid {
-			createIncident(*accessTokens, myCreds, snowReq)
+			createIncident(*accessTokens, c.GetCreds(), helps.SnowReq)
 		} else {
 			l.Println("Token is invalid, code is: ", accessTokens.RequestCode)
 		}
@@ -94,18 +35,18 @@ func main() {
 
 }
 
-func getTokens(c creds) *tokens {
+func getTokens(c helps.Creds) *helps.Tokens {
 	credsString := fmt.Sprintf(
 		"grant_type=%s&client_id=%s&client_secret=%s&username=%s&password=%s",
-		c.grantType, c.clientID, c.clientSecret, c.username, c.password,
+		c.GrantType, c.ClientID, c.ClientSecret, c.Username, c.Password,
 	)
 	payload := strings.NewReader(credsString)
-	req, _ := http.NewRequest("POST", c.oauth_url, payload)
+	req, _ := http.NewRequest("POST", c.Oauth_url, payload)
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	res, _ := http.DefaultClient.Do(req)
-	l.Println("Fetch tokens: ", statusCodes[res.StatusCode])
+	l.Println("Fetch tokens: ", helps.StatusCodes[res.StatusCode])
 	body, _ := io.ReadAll(res.Body)
-	var t tokens
+	var t helps.Tokens
 	json.Unmarshal(body, &t)
 	t.RequestCode = res.StatusCode
 	if res.StatusCode != 200 {
@@ -118,10 +59,10 @@ func getTokens(c creds) *tokens {
 	return &t
 }
 
-func createIncident(a tokens, c creds, snowReq map[string]string) {
+func createIncident(a helps.Tokens, c helps.Creds, snowReq map[string]string) {
 	parmsJson, _ := json.Marshal(snowReq)
 	parmsReader := strings.NewReader(string(parmsJson))
-	req, _ := http.NewRequest("POST", c.incident_url, parmsReader)
+	req, _ := http.NewRequest("POST", c.Incident_url, parmsReader)
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+a.AccessToken)
@@ -131,5 +72,12 @@ func createIncident(a tokens, c creds, snowReq map[string]string) {
 		return
 	}
 	defer res.Body.Close()
-	l.Println("Incident: ", statusCodes[res.StatusCode])
+	// print the body in json
+	body, _ := io.ReadAll(res.Body)
+	// marshal the body into helps.IncidentResponse
+	var incident helps.IncidentResponse
+	json.Unmarshal(body, &incident)
+	l.Println("Incident: ", helps.StatusCodes[res.StatusCode])
+	l.Println("Number: ", incident.Result.Number)
+	l.Println("SysID: ", incident.Result.SysID)
 }
