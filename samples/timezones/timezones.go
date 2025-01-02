ddeb9c55c31430531cf2eac3c64c49b0abcda3da
Rich Davis <vgcrld@gmail.com>
2025-01-02 23:07:18 +0000

    updates

--- samples/timezones/timezones.go
+++ samples/timezones/timezones.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/vgcrld/scoobug/samples/timezones/tz"
 )
 
 func main() {
 
-	p := fmt.Println
-
 	tz.QueryAll()
-	p("Fetched: ", len(tz.ZonesQueried))
-	// p(tz.Errors)
-	tz.ZonesQueried[0].PrintJSON()
+	tz.PrintJSON()
 
 }
--- samples/timezones/tz/base.go
+++ samples/timezones/tz/base.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -16,7 +17,7 @@ var ZonesToFetch = map[string]string{
 	"America/Chicago":  "CST",
 	"America/New_York": "EST",
 	"America/Santiago": "CHL",
-	"Etc/Greenwich":    "GMT",
+	// "Etc/Greenwich":    "GMT",
 	"Europe/Paris":     "CET",
 	"Europe/Bucharest": "EET",
 	"Australia/Sydney": "SYD",
@@ -63,6 +64,7 @@ type Error struct {
 	Err  error
 }
 
+// a list of all errors received
 var Errors []Error
 
 // returns the short form of the long name
@@ -70,17 +72,19 @@ func (t TimezoneInfo) GetShortName() string {
 	return ZonesToFetch[t.Timezone]
 }
 
-func (t TimezoneInfo) PrintJSON() {
-	jsonData, _ := json.MarshalIndent(t, "", "  ")
-	fmt.Println(string(jsonData))
-}
-
 func (t TimezoneInfo) PrintCtmLine() {
 	fmt.Println(t.GetShortName())
 }
 
+// print the JSON
+func PrintJSON() {
+	jsonData, _ := json.MarshalIndent(ZonesQueried, "", "  ")
+	fmt.Println(string(jsonData))
+}
+
 func QueryAll() error {
 	for long := range ZonesToFetch {
+		log.Printf("Querying timezone: %s\n", long)
 		err := QueryZoneAPI(long)
 		if err != nil {
 			return err
@@ -100,11 +104,13 @@ func QueryZoneAPI(longname string) (e error) {
 	req, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
+		log.Println(err)
 		return err
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
+		log.Println(err)
 		return err
 	} else if res.StatusCode != 200 {
 		return errors.New("the server returned a non-200 status code: " + res.Status)
@@ -113,11 +119,13 @@ func QueryZoneAPI(longname string) (e error) {
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
+		log.Println(err)
 		return err
 	}
 
 	err = json.Unmarshal(body, &ret)
 	if err != nil {
+		log.Println(err)
 		Errors = append(Errors, Error{Zone: longname, Err: err})
 	}
 	ZonesQueried = append(ZonesQueried, ret)
